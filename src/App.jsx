deefada75cfee58a2366282ba8afe125e3580dda
Ataur Rehman <rehmangour04@gmail.com>
2024-03-13 17:34:42 +0530

    project finished

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import Main from "./components/main/Main";
+import Sidebar from "./components/sidebar/Sidebar";
+
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
+};
+
+export default App;
